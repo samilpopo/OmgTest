9d1375366d4c15f14ae1b37659f318f66aba3102
BladeMail <blade.mail.ssssamil609@gmail.com>
2024-03-08 17:48:37 +0300

    Initial Commit

---
+++ OmgTest/OmgTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmgTestApp.swift
+//  OmgTest
+//
+//  Created by samil on 08.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct OmgTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
